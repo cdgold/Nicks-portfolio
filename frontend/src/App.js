38a2d984bff07cec8ca2d75ca197a2775e2aebbb
cdgold <98429524+cdgold@users.noreply.github.com>
2023-03-24 14:58:51 -0400

    Setting up file structure

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+const App = () => {
+    return(
+        <div>
+            <p>oh gren world <br></br>
+            don't desert e now
+            </p>
+        </div>
+    )
+}
+
+export default App
